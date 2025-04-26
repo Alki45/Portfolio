dd4e37b5ab4a510f179b5f4f91ff570d8ac02537
alki45 <alikibretmuhamed@gmail.com>
2025-04-26 11:44:43 +0300

    Add script for deployment

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
+import vue from '@vitejs/plugin-vue'
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Portfolio/'  
+    : '/'
+}
