b0e3049e0d7880fb5ce00a06061e80fa571248f5
alki45 <alikibretmuhamed@gmail.com>
2025-04-26 11:57:11 +0300

    adjusting Vue.cofig files

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,6 @@
-import vue from '@vitejs/plugin-vue'
 const { defineConfig } = require('@vue/cli-service')
 
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/Portfolio/'  
-    : '/'
-}
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' ? '/Portfolio/' : '/'
+})
