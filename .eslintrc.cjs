88eba9f80f6d85111837f03c024355df5dcf08d6
alki45 <alikibretmuhamed@gmail.com>
2025-04-26 11:55:31 +0300

    Updates

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+    root: true,
+    env: {
+      node: true,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:vue/vue3-essential',
+    ],
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+    },
+    rules: {
+      // Customize ESLint rules if needed
+    },
+  }
+  
